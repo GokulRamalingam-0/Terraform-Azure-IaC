1ade9ed0f996503b4dbbaa6b6c124d7d8cf4bdbf
GokulRamalingam-0 <gokul.ramalingam00@gmail.com>
2025-11-18 10:23:34 +0530

    Increase OS disk size for default node pool

--- aks-cluster/main.tf
+++ aks-cluster/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name            = "default"
     node_count      = 2
     vm_size         = "Standard_D2s_v3"
-    os_disk_size_gb = 30
+    os_disk_size_gb = 120
   }
 
   identity {

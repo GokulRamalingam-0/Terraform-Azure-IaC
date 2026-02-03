a85070425267414a8ceb0b276af6117436ef7496
GokulRamalingam-0 <gokul.ramalingam00@gmail.com>
2026-02-03 11:37:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "kml_rg_main-8e29a91c850745b8"
+    resource_group_name  = "kml_rg_main-2ccb1823f5dc425a"
     storage_account_name = "tfstate41601"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
